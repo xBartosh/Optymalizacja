402e85444b36071d9e1678cd4d192c15c3b7841b
bartosh <bartekpretnik@gmail.com>
2024-11-15 18:54:21 +0100

    optymalizacja lab1 teoretyczne

---
+++ main.cpp
@@ -0,0 +1,102 @@
+/*********************************************
+Kod stanowi uzupe?nienie materia?ów do ?wicze?
+w ramach przedmiotu metody optymalizacji.
+Kod udost?pniony na licencji CC BY-SA 3.0
+Autor: dr in?. ?ukasz Sztangret
+Katedra Informatyki Stosowanej i Modelowania
+Akademia Górniczo-Hutnicza
+Data ostatniej modyfikacji: 19.09.2023
+*********************************************/
+
+#include"opt_alg.h"
+
+void lab0();
+
+void lab1();
+
+void lab2();
+
+void lab3();
+
+void lab4();
+
+int main() {
+    try {
+        lab1();
+    } catch (string EX_INFO) {
+        cerr << "ERROR:\n";
+        cerr << EX_INFO << endl << endl;
+    }
+    system("pause");
+    return 0;
+}
+
+void lab0() {
+    //Funkcja testowa
+    double epsilon = 1e-2;
+    int Nmax = 10000;
+    matrix lb(2, 1, -5), ub(2, 1, 5), a(2, 1);
+    solution opt;
+    a(0) = -1;
+    a(1) = 2;
+    opt = MC(ff0T, 2, lb, ub, epsilon, Nmax, a);
+    cout << opt << endl << endl;
+    solution::clear_calls();
+
+    //Wahadlo
+    Nmax = 1000;
+    epsilon = 1e-2;
+    lb = 0;
+    ub = 5;
+    double teta_opt = 1;
+    opt = MC(ff0R, 1, lb, ub, epsilon, Nmax, teta_opt);
+    cout << opt << endl << endl;
+    solution::clear_calls();
+
+    //Zapis symulacji do pliku csv
+    matrix Y0 = matrix(2, 1), MT = matrix(2, new double[2]{m2d(opt.x), 0.5});
+    matrix *Y = solve_ode(df0, 0, 0.1, 10, Y0, NAN, MT);
+    ofstream Sout("symulacja_lab0.csv");
+    Sout << hcat(Y[0], Y[1]);
+    Sout.close();
+    Y[0].~matrix();
+    Y[1].~matrix();
+}
+
+void lab1() {
+    // exp
+    double epsilon = 1e-5, gamma = 1e-200;
+    int Nmax = 1000;
+    double d = 1.0;
+    double alpha = 1.5;
+
+    // exp
+    cout << "Optymalizacja funkcji testowej metoda ekspansji:" << endl;
+    double x0 = 100;
+    double *interval = expansion(ff1T, x0, d, alpha, Nmax);
+    double a = interval[0];
+    double b = interval[1];
+    cout << "Przedzial znaleziony metoda ekspansji: [" << a << ", " << b << "]" << endl;
+    cout << "ile fcall = " << solution::f_calls << endl;
+    delete[] interval;
+
+    // fib
+    solution::clear_calls();
+    solution opt;
+    opt = fib(ff1T, a, b, epsilon);
+    cout << "\nMetoda Fibonacciego\n" << opt << endl;
+
+    // lag
+    solution::clear_calls();
+    opt = lag(ff1T, a, b, epsilon, gamma, Nmax);
+    cout << "Metoda Lagrange'a\n" << opt << endl;
+}
+
+void lab2() {
+}
+
+void lab3() {
+}
+
+void lab4() {
+}
---
+++ opt_alg.cpp
@@ -0,0 +1,285 @@
+#include"opt_alg.h"
+
+solution MC(matrix (*ff)(matrix, matrix, matrix), int N, matrix lb, matrix ub, double epsilon, int Nmax, matrix ud1,
+            matrix ud2) {
+    try {
+        solution Xopt;
+        while (true) {
+            Xopt = rand_mat(N);
+            for (int i = 0; i < N; ++i)
+                Xopt.x(i) = (ub(i) - lb(i)) * Xopt.x(i) + lb(i);
+            Xopt.fit_fun(ff, ud1, ud2);
+            if (Xopt.y < epsilon) {
+                Xopt.flag = 1;
+                break;
+            }
+            if (solution::f_calls > Nmax) {
+                Xopt.flag = 0;
+                break;
+            }
+        }
+        return Xopt;
+    } catch (string ex_info) {
+        throw ("solution MC(...):\n" + ex_info);
+    }
+}
+
+double *expansion(matrix (*ff)(matrix, matrix, matrix), double x0, double d, double alpha, int Nmax, matrix ud1,
+                  matrix ud2) {
+    try {
+        double *p = new double[2]{0, 0};
+
+        int i = 0;
+        solution X0(x0);
+        solution X1(x0 + d);
+
+        X0.fit_fun(ff);
+        X1.fit_fun(ff);
+
+        if (X1.y == X0.y) {
+            p[0] = m2d(X0.y);
+            p[1] = m2d(X1.y);
+            return p;
+        }
+
+        if (X1.y > X0.y) {
+            d = -d;
+            X1.x = x0 + d;
+            X1.fit_fun(ff);
+            if (X1.y >= X0.y) {
+                p[0] = m2d(X1.y);
+                p[1] = m2d(X0.y - d);
+                return p;
+            }
+        }
+
+        solution X2;
+        while (solution::f_calls <= Nmax) {
+            ++i;
+            X2.x = x0 + pow(alpha, i) * d;
+            X2.fit_fun(ff);
+
+            if (X2.y > X1.y) break;
+            X0 = X1;
+            X1 = X2;
+        }
+
+        p[0] = (d > 0) ? X0.x(0, 0) : X2.x(0, 0);
+        p[1] = (d > 0) ? X2.x(0, 0) : X0.x(0, 0);
+        return p;
+    } catch (std::string &ex_info) {
+        throw ("double* expansion(...):\n" + ex_info);
+    }
+}
+
+
+solution fib(matrix (*ff)(matrix, matrix, matrix), double a, double b, double epsilon, matrix ud1, matrix ud2) {
+    try {
+        solution Xopt;
+
+        float range = b - a;
+
+        int k = static_cast<int>(ceil(log2(sqrt(5) * range / epsilon) / log2((1 + sqrt(5)) / 2)));
+        vector<double> fibonaccis(k, 1);
+        for (int i = 2; i < k; i++) {
+            fibonaccis[i] = fibonaccis[i - 2] + fibonaccis[i - 1];
+        }
+
+        solution A(a), B(b);
+        solution C(B.x - fibonaccis[k - 2] / fibonaccis[k - 1] * (B.x - A.x));
+        solution D(A.x + B.x - C.x);
+
+        for (int i = 0; i < k - 2; i++) {
+            if (C.fit_fun(ff) < D.fit_fun(ff)) {
+                B = D;
+            } else {
+                A = C;
+            }
+
+            C.x = B.x - fibonaccis[k - i - 2] / fibonaccis[k - i - 1] * (B.x - A.x);
+            D.x = A.x + B.x - C.x;
+        }
+
+        Xopt = (C.y < D.y) ? C : D;
+        Xopt.flag = 1;
+        return Xopt;
+    } catch (std::string ex_info) {
+        throw ("solution fib(...):\n" + ex_info);
+    }
+}
+
+solution lag(matrix (*ff)(matrix, matrix, matrix), double a, double b, double epsilon, double gamma, int Nmax, matrix ud1, matrix ud2) {
+    try {
+        solution Xopt;
+        solution A(a), B(b), C((a + b) / 2), D[2];
+        D[0] = solution(a);
+
+        A.fit_fun(ff);
+        B.fit_fun(ff);
+        C.fit_fun(ff);
+
+        while (true) {
+            matrix l1 = A.y * (pow(B.x, 2) - pow(C.x, 2));
+            matrix l2 = B.y * (pow(C.x, 2) - pow(A.x, 2));
+            matrix l3 = C.y * (pow(A.x, 2) - pow(B.x, 2));
+            double l = m2d(l1 + l2 + l3);
+
+            matrix m1 = A.y * (B.x - C.x);
+            matrix m2 = B.y * (C.x - A.x);
+            matrix m3 = C.y * (A.x - B.x);
+            double m = m2d(m1 + m2 + m3);
+
+            if (m <= 0) {
+                cerr << "m <= 0\n";
+                Xopt = D[0];
+                return Xopt;
+            }
+
+            D[1] = solution(0.5 * l / m);
+            if (A.x < D[1].x && D[1].x < C.x) {
+                if (D[1].fit_fun(ff) < C.y) {
+                    B = C;
+                    C = D[1];
+                } else {
+                    A = D[1];
+                }
+            } else if (C.x < D[1].x && D[1].x < B.x) {
+                if (D[1].fit_fun(ff) < C.y) {
+                    A = C;
+                    C = D[1];
+                } else {
+                    B = D[1];
+                }
+            } else {
+                cerr << "D[1] is outside the interval [A.x, C.x] and [C.x, B.x]\n";
+                Xopt = D[0];
+                return Xopt;
+            }
+
+            if (m2d(B.x - A.x) < epsilon || abs(m2d(D[1].x - D[0].x)) < gamma) {
+                Xopt = D[1];
+                return Xopt;
+            }
+
+            if (solution::f_calls > Nmax) {
+                Xopt = D[1];
+                return Xopt;
+            }
+
+            D[0] = D[1];
+        }
+
+        return Xopt;
+    } catch
+    (string ex_info) {
+        cerr << ex_info;
+        throw ("solution lag(...):\n" + ex_info);
+    }
+}
+
+solution HJ(matrix (*ff)(matrix, matrix, matrix), matrix x0, double s, double alpha, double epsilon, int Nmax,
+            matrix ud1, matrix ud2) {
+    try {
+        solution Xopt;
+        //Tu wpisz kod funkcji
+
+        return Xopt;
+    } catch (string ex_info) {
+        throw ("solution HJ(...):\n" + ex_info);
+    }
+}
+
+solution HJ_trial(matrix (*ff)(matrix, matrix, matrix), solution XB, double s, matrix ud1, matrix ud2) {
+    try {
+        //Tu wpisz kod funkcji
+
+        return XB;
+    } catch (string ex_info) {
+        throw ("solution HJ_trial(...):\n" + ex_info);
+    }
+}
+
+solution Rosen(matrix (*ff)(matrix, matrix, matrix), matrix x0, matrix s0, double alpha, double beta, double epsilon,
+               int Nmax, matrix ud1, matrix ud2) {
+    try {
+        solution Xopt;
+        //Tu wpisz kod funkcji
+
+        return Xopt;
+    } catch (string ex_info) {
+        throw ("solution Rosen(...):\n" + ex_info);
+    }
+}
+
+solution pen(matrix (*ff)(matrix, matrix, matrix), matrix x0, double c, double dc, double epsilon, int Nmax, matrix ud1,
+             matrix ud2) {
+    try {
+        solution Xopt;
+        //Tu wpisz kod funkcji
+
+        return Xopt;
+    } catch (string ex_info) {
+        throw ("solution pen(...):\n" + ex_info);
+    }
+}
+
+solution sym_NM(matrix (*ff)(matrix, matrix, matrix), matrix x0, double s, double alpha, double beta, double gamma,
+                double delta, double epsilon, int Nmax, matrix ud1, matrix ud2) {
+    try {
+        solution Xopt;
+        //Tu wpisz kod funkcji
+
+        return Xopt;
+    } catch (string ex_info) {
+        throw ("solution sym_NM(...):\n" + ex_info);
+    }
+}
+
+solution SD(matrix (*ff)(matrix, matrix, matrix), matrix (*gf)(matrix, matrix, matrix), matrix x0, double h0,
+            double epsilon, int Nmax, matrix ud1, matrix ud2) {
+    try {
+        solution Xopt;
+        //Tu wpisz kod funkcji
+
+        return Xopt;
+    } catch (string ex_info) {
+        throw ("solution SD(...):\n" + ex_info);
+    }
+}
+
+solution CG(matrix (*ff)(matrix, matrix, matrix), matrix (*gf)(matrix, matrix, matrix), matrix x0, double h0,
+            double epsilon, int Nmax, matrix ud1, matrix ud2) {
+    try {
+        solution Xopt;
+        //Tu wpisz kod funkcji
+
+        return Xopt;
+    } catch (string ex_info) {
+        throw ("solution CG(...):\n" + ex_info);
+    }
+}
+
+solution Newton(matrix (*ff)(matrix, matrix, matrix), matrix (*gf)(matrix, matrix, matrix),
+                matrix (*Hf)(matrix, matrix, matrix), matrix x0, double h0, double epsilon, int Nmax, matrix ud1,
+                matrix ud2) {
+    try {
+        solution Xopt;
+        //Tu wpisz kod funkcji
+
+        return Xopt;
+    } catch (string ex_info) {
+        throw ("solution Newton(...):\n" + ex_info);
+    }
+}
+
+solution golden(matrix (*ff)(matrix, matrix, matrix), double a, double b, double epsilon, int Nmax, matrix ud1,
+                matrix ud2) {
+    try {
+        solution Xopt;
+        //Tu wpisz kod funkcji
+
+        return Xopt;
+    } catch (string ex_info) {
+        throw ("solution golden(...):\n" + ex_info);
+    }
+}
