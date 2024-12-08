e2bdb6c2e7f7c72c81d9190d2b6cb2206beadfe5
t-podgorski <147391857+T-Podgorski@users.noreply.github.com>
2024-12-08 17:53:13 +0100

    dokumentacja

--- ode_solver.cpp
+++ ode_solver.cpp
@@ -6,30 +6,45 @@ matrix* solve_ode(matrix(*diff)(double, matrix, matrix, matrix), double t0, doub
 {
 	try
 	{
-		int N = static_cast<int>(floor((tend - t0) / dt) + 1);
-		if (N < 2)
-			throw string("matrix* solve_ode(...):\nprzedzial czasu nie jest zdefiniowany poprawnie");
-		int* s = get_size(Y0);
-		if (s[1] != 1)
-			throw string("matrix* solve_ode(...):\nwarunek poczatkowy musi byc wektorem pionowym");
-		int n = s[0];
+		// liczba kroków
+		int N = static_cast<int>(floor( (tend - t0) / dt ) + 1);
+		if( N < 2 )
+			throw string( "matrix* solve_ode(...):\nprzedzial czasu nie jest zdefiniowany poprawnie" );
+
+		// iloœæ kolumn
+		int* s = get_size( Y0 );
+		if( s[ 1 ] != 1 )
+			throw string( "matrix* solve_ode(...):\nwarunek poczatkowy musi byc wektorem pionowym" );
+
+		// iloœæ wierszy, tj. liczba elementów w wektorze Y
+		int n = s[ 0 ];
 		delete[]s;
-		matrix* S = new matrix[2]{ matrix(N,1), matrix(n,N) };
-		S[0](0) = t0;
-		for (int i = 0; i < n; ++i)
-			S[1](i, 0) = Y0(i);
-		matrix k1(n, 1), k2(n, 1), k3(n, 1), k4(n, 1);
-		for (int i = 1; i < N; ++i)
+
+		// macierz zawieraj¹ca wyniki ka¿dego z N kroków
+		// S[0] - wartoœæ parametru steruj¹cego t w danym kroku
+		// S[1] - rozwi¹zania równañ ró¿niczkowych, tj. stan zmiennych w wektorze Y dla danego t
+		matrix* S = new matrix[ 2 ]{ matrix( N,1 ), matrix( n,N ) };
+
+		// przepisany stan pocz¹tkowy
+		S[ 0 ]( 0 ) = t0;
+		for( int i = 0; i < n; ++i )
+			S[ 1 ]( i, 0 ) = Y0( i );
+
+		// obliczenie wartoœci funkcji w danych krokach metod¹ RK4
+		matrix k1( n, 1 ), k2( n, 1 ), k3( n, 1 ), k4( n, 1 );
+		for( int i = 1; i < N; ++i )
 		{
-			S[0](i) = S[0](i - 1) + dt;
-			k1 = dt * diff(S[0](i - 1), S[1][i - 1], ud1, ud2);
-			k2 = dt * diff(S[0](i - 1) + 0.5 * dt, S[1][i - 1] + 0.5 * k1, ud1, ud2);
-			k3 = dt * diff(S[0](i - 1) + 0.5 * dt, S[1][i - 1] + 0.5 * k2, ud1, ud2);
-			k4 = dt * diff(S[0](i - 1) + dt, S[1][i - 1] + k3, ud1, ud2);
-			for (int j = 0; j < n; ++j)
-				S[1](j, i) = S[1](j, i - 1) + (k1(j) + 2 * k2(j) + 2 * k3(j) + k4(j)) / 6;
+			S[ 0 ]( i ) = S[ 0 ]( i - 1 ) + dt;
+			k1 = dt * diff( S[ 0 ]( i - 1 ), S[ 1 ][ i - 1 ], ud1, ud2 );
+			k2 = dt * diff( S[ 0 ]( i - 1 ) + 0.5 * dt, S[ 1 ][ i - 1 ] + 0.5 * k1, ud1, ud2 );
+			k3 = dt * diff( S[ 0 ]( i - 1 ) + 0.5 * dt, S[ 1 ][ i - 1 ] + 0.5 * k2, ud1, ud2 );
+			k4 = dt * diff( S[ 0 ]( i - 1 ) + dt, S[ 1 ][ i - 1 ] + k3, ud1, ud2 );
+			for( int j = 0; j < n; ++j )
+				S[ 1 ]( j, i ) = S[ 1 ]( j, i - 1 ) + (k1( j ) + 2 * k2( j ) + 2 * k3( j ) + k4( j )) / 6;
 		}
-		S[1] = trans(S[1]);
+
+		// transpozycja - teraz wyniki kolejnych iteracji s¹ w kolumnach zamiast w wierszach
+		S[ 1 ] = trans( S[ 1 ] );
 		return S;
 	}
 	catch (string ex_info)
